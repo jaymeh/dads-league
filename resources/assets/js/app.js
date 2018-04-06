fa5d9c28fbf11373b77f3940e2f0f037c022e339
Jamie Sykes <jamie.sykes@engageinteractive.co.uk>
2018-04-06 13:29:12 +0100

    Move away from vueify since it might not be ideal for use with Laravel

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,23 +5,15 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
 require('./bootstrap');
 
 window.Vue = require('vue');
- 
-Vue.use(Vuetify)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-Vue.component('login', require('./components/Login.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -3,7 +3,6 @@
 		<v-container fluid fill-height>
 			<v-layout align-center justify-center>
 				<v-flex xs12 sm8 md4>
-					
 					<v-container fluid grid-list-md>
 						<v-layout row wrap>
 							<v-flex>
@@ -20,22 +19,26 @@
 							</v-flex>
 						</v-layout>
 					</v-container>
-					
+
 					<v-card class="elevation-12">
-						<v-toolbar dark color="green lighten-1">
-							<v-toolbar-title>Login form</v-toolbar-title>
-							<v-spacer></v-spacer>
-						</v-toolbar>
-						<v-card-text>
-							<v-form>
+						
+					</v-card>
+					<v-card class="elevation-12">
+						<v-form ref="form" method="post" action="/login" lazy-validation v-model="valid">
+							<v-toolbar dark color="green lighten-1">
+								<v-toolbar-title>Login form</v-toolbar-title>
+								<v-spacer></v-spacer>
+							</v-toolbar>
+							<v-card-text>
+								<input type="hidden" name="_token" :value="csrf" />
 								<v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
 								<v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
-							</v-form>
-						</v-card-text>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<v-btn color="green lighten-1 theme--dark">Login</v-btn>
-						</v-card-actions>
+							</v-card-text>
+							<v-card-actions>
+								<v-spacer></v-spacer>
+								<v-btn color="green lighten-1 theme--dark" type="submit">Login</v-btn>
+							</v-card-actions>
+						</v-form>
 					</v-card>
 				</v-flex>
 			</v-layout>
@@ -46,7 +49,9 @@
 <script>
   export default {
     data: () => ({
-      drawer: null
+      drawer: null,
+      valid: true,
+      csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     }),
     props: {
       source: String
