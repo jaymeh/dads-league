ae2cdb240517363b0a9b30c108377064d41f7e41
Jamie Sykes <jamie.sykes@engageinteractive.co.uk>
2018-04-20 13:09:43 +0100

    Move cards to a partial.

--- resources/views/content/picks/index.blade.php
+++ resources/views/content/picks/index.blade.php
@@ -24,26 +24,7 @@
 
                 @foreach($players_with_picks as $player)
 
-                    <div class="column is-3">
-                        <div class="card">
-                            <div class="card-image text-center">
-                                <player-picked-team-image :player-id="{{ $player->id }}"></player-picked-team-image>
-                            </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-content text-center">
-                                    <p class="title is-4">{{ $player->name }}</p>
-                                </div>
-                            </div>
-
-                            @if($player->picks)
-                                <div class="content">
-                                    <team-picker :player-id="{{ $player->id }}"></team-picker>
-                                </div>
-                            @endif
-                        </div>
-                    </div>
-                </div>
+                    @include('partials.picks.pick-card')
 
                 @endforeach
 
---
+++ resources/views/partials/picks/pick-card.blade.php
@@ -0,0 +1,19 @@
+<div class="column is-3">
+    <div class="card">
+        <div class="card-image text-center">
+            <player-picked-team-image :player-id="{{ $player->id }}"></player-picked-team-image>
+        </div>
+    <div class="card-content">
+        <div class="media">
+            <div class="media-content text-center">
+                <p class="title is-4">{{ $player->name }}</p>
+            </div>
+        </div>
+
+        @if($player->picks)
+            <div class="content">
+                <team-picker :player-id="{{ $player->id }}"></team-picker>
+            </div>
+        @endif
+    </div>
+</div>
