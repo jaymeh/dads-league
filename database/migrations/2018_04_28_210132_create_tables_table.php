332133a164d80ed4d6f98dce1e21a2b27151c496
Jamie Sykes <jaymehsykes@gmail.com>
2018-04-28 22:15:43 +0100

    Adds new model for tables

---
+++ database/migrations/2018_04_28_210132_create_tables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('season_id');
+            $table->integer('player_id');
+            $table->integer('score');
+            $table->integer('wins');
+            $table->integer('draws');
+            $table->integer('losses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
