54c9edf7993148b89b6ebccd7d3580bac627cddf
Jamie Sykes <jaymehsykes@gmail.com>
2018-04-30 22:18:12 +0100

    Adds wherehas to query to stop empty results.

--- app/Models/League.php
+++ app/Models/League.php
@@ -20,6 +20,9 @@ class League extends Model
                 'fixtures.homeTeam',
                 'fixtures.awayTeam'
             ])
+            ->whereHas('fixtures', function($q) use($game_date) {
+                $q->whereDate('game_date', $game_date);
+            })
             ->get()
             ->map(function($result) use ($excluded_team_ids) {
                 $should_exclude = $result->fixtures->whereIn('home_team_id', $excluded_team_ids)->whereIn('away_team_id', $excluded_team_ids)->pluck('id');
