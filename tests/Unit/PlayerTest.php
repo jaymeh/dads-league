c225242987aa969428252f737c62aea4e59337d6
Jamie Sykes <jaymehsykes@gmail.com>
2018-05-05 11:44:46 +0100

    Continue working on unit testing for players.

--- tests/Unit/PlayerTest.php
+++ tests/Unit/PlayerTest.php
@@ -2,32 +2,81 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
 class Player extends TestCase
 {
-	public function test_an_unauthenticated_user_cannot_see_player_list()
+	use WithFaker;
+
+	/** @test */
+	public function an_unauthenticated_user_cannot_see_player_list()
 	{
 		// Go to player page when logged out
 		// We should be redirected to the login page
-		$response = $this->get('/players');
+		$response = $this->get(route('players.index'));
 
 		$response
 			->assertRedirect('/login');
 	}
 
-	public function test_an_authenticated_user_can_see_players()
+	/** @test */
+	public function an_authenticated_user_can_see_players()
 	{
 		// Mock a player.
 		$user = factory(\App\Models\User::class)->create();
 		$player = factory(\App\Models\Player::class)->create();
 
 		$response = $this->actingAs($user)
-			->get('/players');
+			->get(route('players.index'));
 
 		$response->assertStatus(200)
 			->assertSee($player->name);
+	}
+
+	/** @test */
+	public function an_unauthenticated_user_cannot_add_players()
+	{
+		$name = $this->faker->name;
+		$email = $this->faker->email;
+
+		$response = $this
+			->post(route('players.store'), [
+				$name,
+				$email
+			]);
+
+		$response->assertRedirect('/login');
+
+		$this->assertDatabaseMissing('players', [
+			'name' => $name,
+			'email' => $email
+		]);
+	}
+
+	/** @test */
+	public function an_authenticated_user_can_add_players()
+	{
+		$user = factory(\App\Models\User::class)->create();
+
+		$name = $this->faker->name;
+		$email = $this->faker->email;
+
+		$response = $this
+			->actingAs($user)
+			->post(route('players.store'), [
+				$name,
+				$email
+			]);
+
+		dd($response->assertRedirect('/players'));
+
+		// $response->assertRedirect('/login');
+
+		$this->assertDatabaseHas('players', [
+			'name' => $name,
+			'email' => $email
+		]);
 	}
 }
