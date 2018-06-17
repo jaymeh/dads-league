b1189430a0fba0b208b0154dd6f0f7e708881cbc
Jaymeh Sykes <jaymehsykes@gmail.com>
2018-06-17 12:07:19 +0100

    Adds test for emails.

---
+++ app/Console/Commands/TestEmail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends out a test email.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Mail::raw('A test email', function($message) {
+            $message->to('jaymehsykes@gmail.com');
+            $message->subject('Test notifications emails.');
+        });
+    }
+}
