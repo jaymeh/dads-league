5cdf86b8078f173a9b5f5063fd9ac8028ff74a63
Jamie Sykes <jaymehsykes@gmail.com>
2018-07-13 22:18:05 +0100

    Adds extra required check.

--- resources/views/partials/picks/dashboard-last-week.blade.php
+++ resources/views/partials/picks/dashboard-last-week.blade.php
@@ -16,41 +16,43 @@
                             <th class="has-text-centered">Player</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        @foreach($player_teams as $player_team)
-                            <tr class="{{ $player_team->gameStatusClass }}">
-                            	<td class="has-text-centered person-name">
-                            		@if($player_team->team_id == $player_team->fixture->home_team_id)
-                            			<strong>{{ $player_team->player->name }}</strong>
-                            		@endif
-                            	</td>
-                                <td class="has-text-centered team-name">{{ $player_team->fixture->homeTeam->name }}</td>
-                                
-                                	@if($player_team->fixture->game)
-                                    	<td class="has-text-centered">
-											<strong>{{ $player_team->fixture->game->home_team_score }}</strong>
-										</td>
-                                	@else
-                                		<td></td>
-                                	@endif
-                                </td>
-                                <td class="has-text-centered">v</td>
-                                <td class="has-text-centered">
-                                	@if($player_team->fixture->game)
-										<strong>{{ $player_team->fixture->game->away_team_score }}</strong>
-                                	@endif
-                                </td>
-                                <td class="has-text-centered team-name">{{ $player_team->fixture->awayTeam->name }}</td>
-                                <td class="has-text-centered person-name">
-                                	@if($player_team->team_id == $player_team->fixture->away_team_id)
-                            			<strong>
-                            				{{ $player_team->player->name }}
-                            			</strong>
-                            		@endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
+                    @if($player_teams && $player_teams->count())
+                        <tbody>
+                            @foreach($player_teams as $player_team)
+                                <tr class="{{ $player_team->gameStatusClass }}">
+                                	<td class="has-text-centered person-name">
+                                		@if($player_team->team_id == $player_team->fixture->home_team_id)
+                                			<strong>{{ $player_team->player->name }}</strong>
+                                		@endif
+                                	</td>
+                                    <td class="has-text-centered team-name">{{ $player_team->fixture->homeTeam->name }}</td>
+                                    
+                                    	@if($player_team->fixture->game)
+                                        	<td class="has-text-centered">
+    											<strong>{{ $player_team->fixture->game->home_team_score }}</strong>
+    										</td>
+                                    	@else
+                                    		<td></td>
+                                    	@endif
+                                    </td>
+                                    <td class="has-text-centered">v</td>
+                                    <td class="has-text-centered">
+                                    	@if($player_team->fixture->game)
+    										<strong>{{ $player_team->fixture->game->away_team_score }}</strong>
+                                    	@endif
+                                    </td>
+                                    <td class="has-text-centered team-name">{{ $player_team->fixture->awayTeam->name }}</td>
+                                    <td class="has-text-centered person-name">
+                                    	@if($player_team->team_id == $player_team->fixture->away_team_id)
+                                			<strong>
+                                				{{ $player_team->player->name }}
+                                			</strong>
+                                		@endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    @endif
                 </table>
 			</div>
 		</div>
